706f516a9ca7c57be22ef845ab36fc8d215db5f8
yashgarg7302 <yashgarg7302@gmail.com>
2024-03-05 12:02:51 +0530

    memo

---
+++ memo.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import { memo } from 'react';
+
+function App() {
+  const [firstTitle, setFirstTitle] = useState("my name is harkirat");
+
+  function changeTitle() {
+    setFirstTitle("My name is " + Math.random())
+  }
+
+  return (
+    <div>
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={firstTitle} />
+      <br />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+    </div>
+  )
+}
+
+const Header = memo(function ({title}) {
+  return <div>
+    {title}
+  </div>
+})
+
+export default App
